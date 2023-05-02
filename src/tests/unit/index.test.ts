cdd64313b22451228e1aa4658461d6b2c61f75af
Loadigjablos <loadingjablowme@gmail.com>
2023-05-02 08:06:02 +0200

    adding

---
+++ src/tests/unit/index.test.ts
@@ -0,0 +1,13 @@
+import { BankAccount, IBankAccount } from '../../bank/bank-account'
+
+describe('Ingredient DAL', () => {
+    beforeAll(async () => {
+
+    })
+
+    describe('', () => {
+        it('s', async () => {
+
+        })
+    })
+})
